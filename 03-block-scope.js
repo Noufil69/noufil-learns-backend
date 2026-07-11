1ab7f6611674d434272446ce12f2b5050feb65f7
Noufil Naveed <naveednoufil@gmail.com>
2026-07-11 15:43:09 +0500

    feat: add block scope lesson 03

---
+++ 03-block-scope.js
@@ -0,0 +1,13 @@
+const weather="rain";
+if(weather==="rain"){
+    const umbrella="opened";
+    let jacket="on";
+    var shoes="wet";
+    console.log("inside if block:");
+    console.log(umbrella);
+    console.log(jacket);
+    console.log(shoes);
+}
+console.log("outside if vlock");
+console.log(shoes);
+console.log(umbrella);
