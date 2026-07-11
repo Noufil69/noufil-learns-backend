8b0d68590aaa26259a1b06729fac2b3d95cfda52
Noufil Naveed <naveednoufil@gmail.com>
2026-07-11 15:17:58 +0500

    feat: add variable lesson 01

---
+++ 01-variables.js
@@ -0,0 +1,5 @@
+var name  = " Noufil";
+let age = 23;
+const country = "Pakistan";
+const greeting = "Hello, from" +  name + "! You are " + age + " years old and live in " + country + ".";
+console.log(greeting);
