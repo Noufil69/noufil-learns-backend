5a679240d662cea7c97496459d24995c4dd61a79
Noufil Naveed <naveednoufil@gmail.com>
2026-07-11 15:29:08 +0500

    feat: add scope lesson 02

---
+++ 02-scope.js
@@ -0,0 +1,9 @@
+const globalVar= "I am everywhere";
+function myRoom(){
+    const roomVar="I only exist in this room";
+    console.log(globalVar);
+    console.log(roomVar);
+}
+myRoom();
+console.log(globalVar);
+
