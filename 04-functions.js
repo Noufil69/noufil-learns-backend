11ad519f72c1bc9a76e5789fbc7f28f776929d31
Noufil Naveed <naveednoufil@gmail.com>
2026-07-12 17:35:44 +0500

    feat:add functions lesson 04

---
+++ 04-functions.js
@@ -0,0 +1,10 @@
+function greet (name){
+    return "Hello,"+name;
+}
+const add=function(a,b){
+    return a+b;
+}
+const multiply=(a,b)=>a*b;
+console.log(greet("Noufil"));
+console.log(add(6,9));
+console.log(multiply(4,20));
